c943d270a64bbb1287a5d23006c99d21ea9868be
Propsih <80388076+Propsih@users.noreply.github.com>
2021-04-12 11:53:45 +0300

    Update Query For OWOX.sql

--- Query For OWOX.sql
+++ Query For OWOX.sql
@@ -1,10 +1,31 @@
+WITH meow AS
+(SELECT sessionId
+,MAX(hits.time)/1000 AS duration
+,MAX(totals.pageviews) AS pageviews
+,SUM(transaction.transactionRevenue) AS revenue
+,MAX(totals.transactions) AS cnt_transactions
+,MAX(totals.events) as events
+FROM `project_name.dataset_name.ga_sessions_*`
+,UNNEST(hits) AS hits
+WHERE 1=1
+AND _TABLE_SUFFIX BETWEEN '20210101' AND '20210131'
+GROUP BY sessionId)
+
+
+	
 SELECT
 
 --инфа о сессии/клиенте
 clientId
-,sessionId
-newVisits     --1 when new
+,a.sessionId
+,newVisits     --1 when new
 ,date
+,b.duration
+,b.pageviews
+,CASE WHEN b.events<=1 THEN 1
+ELSE 0 END as bounce
+,b.revenue
+,b.cnt_transactions
 
 
 --устройство
@@ -40,6 +61,10 @@ newVisits     --1 when new
 ,customDimensions.value
 
 
+--эксперимент
+/*,experiment.experimentId
+,experiment.experimentVariant*/
+
 --промо
 /*,hits.promotion.promoCreative
 ,hits.promotion.promoId
@@ -47,9 +72,11 @@ newVisits     --1 when new
 ,hits.promotion.promoPosition
 ,hits.promotionActionInfo*/
 
-FROM 'project_name:dataset_name.owoxbi_sessions_*' a
+FROM `project_name:dataset_name.owoxbi_sessions_*` a
+LEFT JOIN meow b ON a.sessionId=b.sessionId
 ,UNNEST(hits) AS hits
 ,UNNEST(hits.customDimensions) as customDimensions
 WHERE 1=1
 AND _TABLE_SUFFIX BETWEEN '20210101' AND '20210131'
+
 
