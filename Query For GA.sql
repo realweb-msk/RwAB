6257d03f13c7074cdf89fe570cd9bfe4987e3ee6
Propsih <80388076+Propsih@users.noreply.github.com>
2021-04-12 11:52:51 +0300

    Update Query For GA.sql

--- Query For GA.sql
+++ Query For GA.sql
@@ -1,10 +1,32 @@
+WITH meow AS
+(SELECT visitId
+,MAX(hits.time)/1000 AS duration
+,MAX(totals.pageviews) AS pageviews
+,MAX(totals.bounces	) AS bounce
+,MAX(totals.totalTransactionRevenue	)/1000000 AS revenue
+,MAX(totals.transactions) AS cnt_transactions
+FROM `project_name.dataset_name.ga_sessions_*`
+,UNNEST(hits) AS hits
+WHERE 1=1
+AND _TABLE_SUFFIX BETWEEN '20210101' AND '20210131'
+GROUP BY visitId)
+
+
+
+
+
+
 SELECT
 --инфа о сессии/клиенте
 fullVisitorId
-,visitId
+,a.visitId
 ,totals.newVisits       --1 when new
 ,date
-
+,b.duration
+,b.pageviews
+,b.bounce
+,b.revenue
+,b.cnt_transactions
 
 --устройство
 ,device.deviceCategory
@@ -36,6 +58,10 @@ fullVisitorId
 ,customDimensions.index
 ,customDimensions.value
 
+--эксперимент
+/*,hits.experiment.experimentId
+,hits.experiment.experimentVariant*/
+
 
 --промо
 /*,promotion.promoCreative
@@ -45,9 +71,10 @@ fullVisitorId
 ,promotionActionInfo.promoIsView
 ,promotionActionInfo.promoIsClick*/
 
-FROM 'project_name:dataset_name.ga_sessions_*'
+FROM `project_name.dataset_name.ga_sessions_*` a
 ,UNNEST(hits) AS hits
 ,UNNEST(hits.customDimensions) AS customDimensions
+LEFT JOIN meow b ON a.visitId=b.visitId
 /*,UNNEST(hits.promotion) as promotion
   ,UNNEST(hits.promotionActionInfo) as promotionActionInfo*/
 
