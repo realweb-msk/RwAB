26bbef1564ec6ea1399757d730a3d36f52095b34
FreedomSlow <60659176+FreedomSlow@users.noreply.github.com>
2021-05-31 18:51:34 +0300

    Update visual.py

--- core/visual.py
+++ core/visual.py
@@ -2,6 +2,7 @@ from itertools import combinations
 import plotly.graph_objs as go
 import plotly.express as px
 import plotly.figure_factory as ff
+import scipy.stats as st
 import logging
 
 
@@ -26,7 +27,7 @@ class ShowPlots:
         for comb in vars_comb:
             df_1 = df.query(f"{variant_col} == {comb[0]}").reset_index(drop=True)
             df_2 = df.query(f"{variant_col} == {comb[1]}").reset_index(drop=True)
-            plt_data = df_1[metric].cumsum() - df_2[metric].cumsum()
+            plt_data = df_2[metric].cumsum() - df_1[metric].cumsum()
             figures.append(go.Scatter(y=plt_data, x=df_1[date_col],
                                       name=f"{metric} lift variant {comb[1]} vs {comb[0]}"))
 
@@ -37,35 +38,44 @@ class ShowPlots:
         return
 
     @staticmethod
-    def time_series(df, date_col, variant_col, metric, rolling_period=None, width=None, height=None):
+    def time_series(df, date_col, variant_col, metric, rolling_period=None, width=None, height=None, alpha=.95):
 
         _vars = df[variant_col].unique()
         dfs = [df.query(f"{variant_col} == {var}") for var in _vars]
         rolling_period = 7 if rolling_period is None else rolling_period
 
-        figures = [
-            go.Scatter(y=d.sort_values(by=date_col)[metric],
-                   name=f'Experiment variant {var}',
-                   x=d.sort_values(by=date_col)[date_col]
-                   )
-            for d, var in zip(dfs, _vars)
-        ]
-
-        means = []
+        figures = []
         for d, var in zip(dfs, _vars):
-            means.append(go.Scatter(
+            # Mean line
+            figures.append(go.Scatter(
                 y=[d[metric].mean()] * len(d),
                 name=f'Mean of variant {var}',
                 x=d.sort_values(by=date_col)[date_col],
                 line={'dash': 'dash'}))
 
-            means.append(go.Scatter(
-                y=d[metric].rolling(rolling_period).mean(),
+            rolling_mean = d[metric].rolling(rolling_period).mean()
+            # Rolling mean
+            figures.append(go.Scatter(
+                y=rolling_mean,
                 x=d.sort_values(by=date_col)[date_col],
-                line={'dash': 'dot'},
-                name=f'rolling mean {rolling_period} variant {var}'))
+                line={'dash': 'solid'},
+                name=f'rolling mean {rolling_period} variant {var}')
+            )
+            # Conf. intervals
+            figures.append(go.Scatter(
+                y=rolling_mean * .95,
+                x=d.sort_values(by=date_col)[date_col],
+                line={'dash': 'longdashdot'},
+                name=f'Conf. interval upper bound {rolling_period} variant {var}')
+            )
 
-        figures += means
+            figures.append(go.Scatter(
+                y=rolling_mean * 1.05,
+                x=d.sort_values(by=date_col)[date_col],
+                line={'dash': 'longdashdot'},
+                name=f'Conf. interval lower bound {rolling_period} variant {var}')
+            )
+
         fig = go.Figure(figures)
 
         if width is None and height is None:
@@ -75,6 +85,40 @@ class ShowPlots:
             fig.update_layout(autosize=False, width=width, height=height, title='Variants comparison')
 
         fig.show()
+        return
+
+    @staticmethod
+    def p_value_dynamic(df, date_col, variant_col, metric, alpha=0.05, **kwargs):
+        df = df.sort_values(by=date_col)
+        _vars = df[variant_col].unique()
+        vars_comb = combinations(df[variant_col].unique(), 2)
+
+        figures = []
+
+        for comb in vars_comb:
+            df_1 = df.query(f"{variant_col} == {comb[0]}")
+            df_2 = df.query(f"{variant_col} == {comb[1]}")
+            p_values = []
+
+            for i in range(1, len(df_1)):
+                p_value = st.mannwhitneyu(df_1[metric][:i], df_2[metric][:i])[1]
+                p_values.append(p_value)
+
+            figures.append(go.Scatter(
+                x=df_1[date_col],
+                y=p_values,
+                name=f'P-values for variants {comb}'
+            ))
+
+        fig = go.Figure(figures, **kwargs)
+        fig.add_trace(go.Scatter(
+            x=df[date_col].unique(),
+            y=[alpha]*len(df[date_col].unique()),
+            name=f'Alpha-value = {alpha}'
+        ))
+        fig.update_layout(title=f'p_value dynamic for {metric}')
+        fig.show()
+
         return
 
     @staticmethod
@@ -107,6 +151,6 @@ class ShowPlots:
             self.distplots(self.df, self.variant_col, metric)
             self.boxplots(self.df, self.variant_col, metric)
             self.cumulative_lift(self.df, self.date_col, self.variant_col, metric)
-
+            self.p_value_dynamic(self.df, self.date_col, self.variant_col, metric)
         return
 
